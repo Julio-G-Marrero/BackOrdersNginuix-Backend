93bd364470d6b8cb0a66e2dbe4651be30e782308
Julio <julio.garcia135@hotmail.com>
2025-02-17 13:07:32 -0600

    s2

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -12,5 +12,29 @@ router.put('/users/:id', protect, adminController.updateUser);
 router.delete('/users/:id', protect, adminController.deleteUser);
 router.patch("/users/:id/restrict", protect, adminController.restrictUserAccess);
 router.patch('/users/:id/restore', protect, adminController.restoreUserAccess);
+router.post("/create-admin", async (req, res) => {
+    try {
+        // Verifica si ya existe un admin
+        const adminExists = await User.findOne({ role: "admin" });
+        if (adminExists) {
+            return res.status(400).json({ message: "El usuario admin ya existe." });
+        }
 
+        // Crear usuario admin
+        const hashedPassword = await bcrypt.hash("12345678", 10);
+        const admin = new User({
+            name: "Admin",
+            email: "admin@email.com",
+            password: hashedPassword,
+            role: "admin",
+            status: "active"
+        });
+
+        await admin.save();
+        res.status(201).json({ message: "Usuario admin creado con éxito." });
+    } catch (error) {
+        console.error("Error creando admin:", error);
+        res.status(500).json({ message: "Error en el servidor." });
+    }
+});
 module.exports = router;
