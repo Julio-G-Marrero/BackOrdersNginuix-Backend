d5f651d678dc8bc24b5111e024ab531d5367893d
Julio <julio.garcia135@hotmail.com>
2025-02-19 20:04:39 -0600

    whatsapp api

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const twilio = require('twilio');
 
 // Configurar cliente de Twilio
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = twilio("ACa5af537e7de8d375fc557c8417d8fb4a", "f8f7a2e9638c787f698e5dab72a4273f");
 
 // Función para enviar mensajes de WhatsApp con una plantilla
 const sendWhatsAppMessage = async (to, variables) => {
